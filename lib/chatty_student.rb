88b59998275cf90519bf57fe96f5294a3cbdeaaa
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-09-24 07:57:53 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,13 +6,6 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   def raise_hand
-    i = 0
-    loop do
-      i += 1
-      puts super
-      if i === 10
-        break
-      end
-    end
+    10.times super
   end
 end
